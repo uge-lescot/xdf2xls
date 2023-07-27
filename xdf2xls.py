2b6fe0a977befafc1f6e4e90376fc213886e8ad7
Bertrand Richard <bertrand.richard@univ-eiffel.fr>
2023-07-27 17:11:55 +0200

    Fixed saving not working

--- xdf2xls.py
+++ xdf2xls.py
@@ -7,21 +7,24 @@ import numpy as np
 
 def xdf2xls(xdf_file: str) -> None:
     data, header = pyxdf.load_xdf(xdf_file)
-    writer = pd.ExcelWriter(f"{xdf_file}.xls", engine='xlsxwriter')
-    for stream in data:
-        df = pd.DataFrame()
-        df['timestamps'] = stream['time_stamps']
-        y = stream['time_series']
-        descs = stream['info']['desc'][0]['channels'][0]['channel']
-        if isinstance(y, list):
-            for yy, label in zip(zip(*y), descs):
-                df[label['label'][0]] = yy
-        elif isinstance(y, np.ndarray):
-            df = df.join(pd.DataFrame(y))
-            names = {i: label['label'][0] for i, label in enumerate(descs)}
-            df.rename(columns=names, inplace=True)
-        df.to_excel(writer, sheet_name=stream['info']['name'][0], startrow=0, startcol=0)
-    writer.save()
+    with pd.ExcelWriter(f"{xdf_file}.xls", engine='xlsxwriter') as writer:
+        for stream in data:
+            df = pd.DataFrame()
+            df['timestamps'] = stream['time_stamps']
+            y = stream['time_series']
+            try:
+                descs = stream['info']['desc'][0]['channels'][0]['channel']
+            except IndexError:
+                print(f"Failed to find channels for {stream['info']['name'][0]}")
+                continue
+            if isinstance(y, list):
+                for yy, label in zip(zip(*y), descs):
+                    df[label['label'][0]] = yy
+            elif isinstance(y, np.ndarray):
+                df = df.join(pd.DataFrame(y))
+                names = {i: label['label'][0] for i, label in enumerate(descs)}
+                df.rename(columns=names, inplace=True)
+            df.to_excel(writer, sheet_name=stream['info']['name'][0], startrow=0, startcol=0)
 
 
 def main():
